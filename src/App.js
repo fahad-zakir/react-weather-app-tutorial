d18f379d947b69fa859f99a074275e3c2640a0ca
fahad-zakir <fahadzakir81@gmail.com>
2018-05-21 13:26:02 -0400

    updated api with imperial units and added props to form

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React, { Component } from 'react';
 
 import Titles from "./components/Titles";
 import Form from "./components/Form";
-import Weather from "./components/Weather";
+import Weather from "./components/Weather.js";
 
 const API_KEY = "94da788e62953643710eabd7b69152e4";
 
 class App extends Component {
-  getWeather = async () => {
-    const api_call = await fetch(`http://samples.openweathermap.org/data/2.5/weather?zip=32836,us&appid=${API_KEY}`);
+  getWeather = async (e) => {  
+    //e is an event object and will prevent the page reload that's stopping the data
+    e.preventDefault();
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?zip=32836,us&appid=${API_KEY}&units=imperial`);
     const data = await api_call.json();
     console.log(data);
   }
@@ -16,7 +18,7 @@ class App extends Component {
     return (
       <div>
         <Titles />
-        <Form />
+        <Form getWeather={this.getWeather}/>
         <Weather />
       </div>
     );
