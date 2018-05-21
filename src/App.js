41557a889b73c91656108d23218a58518cf03168
fahad-zakir <fahadzakir81@gmail.com>
2018-05-21 13:02:46 -0400

    added an API key and it's functions

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
 
+const API_KEY = "94da788e62953643710eabd7b69152e4";
+
 class App extends Component {
+  getWeather = async () => {
+    const api_call = await fetch(`http://samples.openweathermap.org/data/2.5/weather?zip=32836,us&appid=${API_KEY}`);
+    const data = await api_call.json();
+    console.log(data);
+  }
   render() {
     return (
       <div>
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 
 class Form extends Component {
     render() {
-        return (
-            <div>
-                Form Component
-            </div>
-        );
+        return <div>
+            <form>
+              <input type="text" name="city" placeholder="City..." />
+              <input type="text" name="country" placeholder="Country..." />
+              <button>Get Weather</button>
+            </form>
+          </div>;
     }
 }
 
