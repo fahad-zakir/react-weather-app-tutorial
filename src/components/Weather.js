1593a301795c3c4826fd546b20bd9ba063b6d478
fahad-zakir <fahadzakir81@gmail.com>
2018-05-22 00:26:16 -0400

    modified weather component

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,7 +4,11 @@ class Weather extends Component {
   render() {
     return (
     <div>
+        { this.props.city }
+        { this.props.country }
         { this.props.temperature }
+        { this.props.humidity }
+        { this.props.description }
     </div>
     );
   }
