2f005e29e1366c1d4b50d61c719498931bbbed8e
fahad-zakir <fahadzakir81@gmail.com>
2018-05-24 15:36:44 -0400

    converted Form component into a statelss component

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
-//porps have to be thrown in as an argument in order for props to work and notice how
-//this. was removed in a stateless component
-const Weather = (props) => {
-    return (
+//porps have to be thrown in as an argument in order for props to work since it's not defined with it being in the argument
+//and notice how this. was removed in a stateless component
+
+// if you are only returning one argument in an arrow function you can get rid of the surrounding parenthesis
+const Weather = props => (
+        //this div block is the only element, not all the props
+        //if you are only returning one element in an arrow function, like in here we are doing it with this setup 
+        //of const otherwise look below in comments for the mulitple elements method
         <div>
             {props.city && props.country && <p>Location: {props.city}, {props.country}</p>}
             {props.temperature && <p>Temperature: {props.temperature}</p>}
@@ -10,8 +14,7 @@ const Weather = (props) => {
             {props.description && <p>Coditions: {props.description}</p>}
             {props.error && <p>{props.error}</p>}
         </div>
-    )
-}
+    );
 // conditional operators from { this expression } what line 7 means is, if this.props.city returns true, 
 // and this.props.country returns true only then do we want what's next
 // if statements 
@@ -34,3 +37,23 @@ export default Weather;
 /// this way is a state component and the one above with the const
 /// is a stateless component that does not change states and is for UI only
 // since the state is not changing we do not need a state component, and can use a stateless to write cleaner codes
+
+// const Weather = props => {
+//   return (
+    //this div block is the only element, not all the props
+    //if you are only returning one element, like in here we are doing it with this setup
+    //of const otherwise look below in comments for the mulitple elements method
+//     <div>
+//       {props.city &&
+//         props.country && (
+//           <p>
+//             Location: {props.city}, {props.country}
+//           </p>
+//         )}
+//       {props.temperature && <p>Temperature: {props.temperature}</p>}
+//       {props.humidity && <p>Humidity: {props.humidity}</p>}
+//       {props.description && <p>Coditions: {props.description}</p>}
+//       {props.error && <p>{props.error}</p>}
+//     </div>
+//   );
+// };
