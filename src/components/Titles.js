008e5a39e2ee959e9e6faebd2c760184f53d650c
fahad-zakir <fahadzakir81@gmail.com>
2018-05-24 15:38:06 -0400

    updated Titles component to a stateless component

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 
-class Titles extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Weather Finder</h1>
-                <p>Find out temperature, conditions and more...</p>
-            </div>
-        );
-    }
-}
+const Titles = props => (
+    <div>
+        <h1>Weather Finder</h1>
+        <p>Find out temperature, conditions and more...</p>
+    </div>
+);
 
 export default Titles;
+
+// look at weather component for notes on this stateless component which starts with const
