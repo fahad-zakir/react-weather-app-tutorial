4aedf444eb98fc3047f48c6c6ff0670749029a83
fahad-zakir <fahadzakir81@gmail.com>
2018-05-26 00:07:37 -0400

    added the app.css from tutorial and classes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather.js";
@@ -18,11 +18,11 @@ class App extends Component {
     humidity: undefined,
     description: undefined,
     error: undefined
-  }
-  getWeather = async (e) => {  
+  };
+  getWeather = async e => {
     //e is an event object and will prevent the page reload that's stopping the data (default behavior); e.prevent signifies single page app what React is all about
     e.preventDefault();
-    // we created this city & country var to target the form city and change it's value ; we removed the default city and state 
+    // we created this city & country var to target the form city and change it's value ; we removed the default city and state
     // from the api and set the props there as well with the $ sign
 
     // e.target is for the argument of the event handler in the form, events are objects with certain properties
@@ -30,7 +30,9 @@ class App extends Component {
     // Thus e.target.value is the value property of some DOM element, in this case that means the text entered in the search input.
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`);
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`
+    );
     const data = await api_call.json();
     // we have stored the api_call in the data variable since we also want to convert it to a json
 
@@ -38,19 +40,18 @@ class App extends Component {
     // if it works then we want the code to work and render these properties
     if (city && country) {
       this.setState({
-      temperature: data.main.temp,
-      city: data.name,
-      country: data.sys.country,
-      humidity: data.main.humidity,
-      description: data.weather[0].description,
-      error: ""
-    });
-  
-} else {
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+    } else {
       this.setState({
         // do this if it's a blank form and you try to hit submit
         // this is so we can change and manipulate data below (change the state), we got the intial state (data) from the api
-        // it came in the json called as it is below 
+        // it came in the json called as it is below
         temperature: undefined,
         city: undefined,
         country: undefined,
@@ -59,19 +60,32 @@ class App extends Component {
         error: "Please enter the value."
       });
     }
-  }
+  };
   // The state is set above and therefore rendered below for the Form and weather component to recieve data from .props
   render() {
     return (
       <div>
-        <Titles />
-        <Form getWeather={this.getWeather}/>
-        <Weather 
-        temperature={this.state.temperature}
-        city={this.state.city}
-        country={this.state.country}
-        description={this.state.description}
-        error={this.state.error} />
+        <div className="wrapper">
+          <div className="main">
+            <div className="container-fluid">
+              <div className="row">
+                <div className="col-xs-5 title-container">
+                  <Titles />
+                </div>
+                <div className="col-xs-7 form-container">
+                  <Form getWeather={this.getWeather} />
+                  <Weather
+                    temperature={this.state.temperature}
+                    city={this.state.city}
+                    country={this.state.country}
+                    description={this.state.description}
+                    error={this.state.error}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
